e19d7ca79b9fb6bbbec891dab730cce7b70248e6
Javier Urbina Rathgeb <urbinarathgeb@gmail.com>
2026-04-19 10:53:31 -0400

    Implementación ruta /contact y su respectiva view.

--- src/routes/juegos.routes.js
+++ src/routes/juegos.routes.js
@@ -5,27 +5,42 @@ const router = Router();
 
 router.get('/', (req, res) => {
 	const featuredGames = data.filter(juego => juego.isFeatured);
-	res.render('index', {games: featuredGames});
+	res.render('index', {
+		title: 'Juegos destacados',
+		description: 'Descubre tu próxima aventura épica entre nuestra selección de juegos',
+		games: featuredGames
+	});
 });
 
-router.get('/games', (req, res) => res.render('games', {games: data})
+router.get('/games', (req, res) => res.render('games', {
+		title: 'Todos los juegos',
+		games: data
+	})
 );
 
 router.get('/games/:id', (req, res) => {
 	const id = parseInt(req.params.id);
 	const game = data.find(game => game.id === id);
 
-	if(!game) return res.status(404).send('Juego no encontrado');
+	if (!game) return res.status(404).send('Juego no encontrado');
 
-	console.log(game)
-	res.render('game', {game})
+	res.render('gameDetail', {game})
+});
+
+router.get('/contact', (req, res) => {
+	res.render('contact', {
+		title: 'Contacto',
+		message: 'Puedes contactarnos enviando un correo a contacto@gamevibe.com'
+	});
 });
 
 router.get('/about', (req, res) => {
-	const data = {
-		name : 'Javier Urbina'
-	}
-	res.render('about', {data});
+
+	res.render('about', {
+		title: 'Sobre el proyecto',
+		name: 'Javier Urbina',
+		description: 'Este es un proyecto de aprendizaje del Bootcamp Fullstack JavaScrip de Talento Digital, correspondiente a la AE4 de la clase número 10 del módulo 6: Desarrollo de aplicaciones web Node Express. '
+	});
 })
 
 export default router;
